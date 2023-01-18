0b7d3ae8613aac8e428a74882c0af66fd77325f1
Carlos J Hernandez <carlos.hernandez@infosel.com>
2023-01-18 17:42:29 +0300

    refactor the files comparison to a script

--- bmv_utils/compare.py
+++ bmv_utils/compare.py
@@ -1,41 +1,9 @@
-#! /usr/bin/env python
-"""Compare two json files, one is the expected output, the other is the actual output.
-Whenever you find a timestamp, compare it with the dateutil.parser library functions, use asserts to compare the two.
 """
-
+Comparisons for all the different
+"""
 import sys
 import dateutil.parser
 import json
-
-def compare_bmv_producto_jsons(expected_output, actual_output):
-    """Compare two  bmv producto json files, one is the expected output, the other is the actual output.
-    Whenever you find a timestamp, compare it with the dateutil.parser library functions, 
-    otherwise, use asserts to compare the two.
-    """
-    with open(expected_output, 'r') as expected_file:
-        with open(actual_output, 'r') as actual_file:
-            '''Read each line from expected_output and actual_output, compare them.'''
-            for expected_line in expected_file:
-                actual_line = actual_file.readline()
-                '''Compare each field in the line, if it is a timestamp, use dateutil.parser to parse it and compare.'''
-                expected_json = json.loads(expected_line)
-                try:
-                    actual_json = json.loads(actual_line)
-                    # Key is important because is the way to identfy the message received from BMV
-                    assert 'key' in actual_json, f"'key' not in actual_output file: {actual_output}"
-                    # tipoMensaje is important because is the way we can identify the type of message received from BMV and its fields.
-                    assert 'tipoMensaje' in actual_json, f"'tipoMensaje' not in actual_output file: {actual_output}"
-                    if expected_json['tipoMensaje'] == 'ca':
-                        compare_bmv_catalogo_ca(expected_json, actual_json)
-                    elif expected_json['tipoMensaje'] == 'P':
-                        compare_bmv_message_P(expected_json, actual_json)
-                    else:
-                        print(f"{expected_json['key']}: Unknown message type: {expected_json['tipoMensaje']}")
-                except AssertionError as e:
-                    print(f"Assertion failed: {e}")
-                except:
-                    print(f"{expected_json['key']}: An error ocurred")
-
 
 
 def assert_eq_json_field(expected_json: dict, actual_json: dict, field: str) -> None:
@@ -51,6 +19,7 @@ def compare_bmv_mensaje_M(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'volatilidad')
     return True
 
+
 def compare_bmv_mensaje_H(expected: dict, actual: dict) -> bool:
     key = actual['key']
     assert_eq_json_field(expected, actual, 'key')
@@ -58,6 +27,7 @@ def compare_bmv_mensaje_H(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'tipoMensaje')
     assert_eq_json_field(expected, actual, 'folioHecho')
     return True
+   
     
 def compare_bmv_mensaje_O(expected: dict, actual: dict) -> bool:
     key = actual['key']
@@ -68,6 +38,7 @@ def compare_bmv_mensaje_O(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'sentido')
     assert_eq_json_field(expected, actual, 'tipo')
     return True
+   
     
 def compare_bmv_mensaje_E(expected: dict, actual: dict) -> bool:
     key = actual['key']
@@ -82,6 +53,7 @@ def compare_bmv_mensaje_E(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'promedio')
     assert_eq_json_field(expected, actual, 'last')
     return True
+
 
 def compare_bmv_message_P(expected: dict, actual: dict) -> bool:
     '''Compare two p messages, one is the expected output, the other is the actual output.'''
@@ -168,7 +140,6 @@ def compare_bmv_catalogo_cf(expected: dict, actual: dict) -> bool:
     return True
 
 
-
 def compare_bmv_catalogo_cc(expected: dict, actual: dict) -> bool:
     key = actual['key']
     assert_eq_json_field(expected, actual, 'key')
@@ -186,7 +157,6 @@ def compare_bmv_catalogo_cc(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'ISIN')
     assert_eq_json_field(expected, actual, 'bolsaOrigen')
     return True
-
 
 
 def compare_bmv_catalogo_cb(expected: dict, actual: dict) -> bool:
@@ -272,17 +242,3 @@ def compare_bmv_catalogo_cg(expected: dict, actual: dict) -> bool:
     assert_eq_json_field(expected, actual, 'periodicidad')
     assert_eq_json_field(expected, actual, 'numeroVencimientos')
     return True
-
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('[Error] Usage: compare_producto40_json.py expected.json actual.json')
-        print('Compare two json files, one is the expected output, the other is the actual output.')
-        sys.exit(1)
-    expected_output = sys.argv[1]
-    actual_output = sys.argv[2]
-    compare_bmv_producto_jsons(expected_output, actual_output)
-            
-            
-    
